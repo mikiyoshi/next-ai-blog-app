2ce5f84dbb7cff9eb5b28940e12eec4eb91df15f
mikiyoshi <mikiyoshi.kokura@gmail.com>
2023-04-27 16:43:47 -0400

    Edit, Cancel, Submit form to prisma API

--- blog-ai-app/app/post/[id]/Content.tsx
+++ blog-ai-app/app/post/[id]/Content.tsx
@@ -1,12 +1,13 @@
 'use client';
 import SocialLinks from '@/app/(shared)/SocialLinks';
 import { FormattedPost } from '@/app/types';
-import { XMarkIcon, PencilSquareIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 import React, { useState } from 'react';
 import { Editor, EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import EditorMenuBar from './EditorMenuBar';
+import CategoryAndEdit from './CategoryAndEdit';
+import Article from './Article';
 
 type Props = {
   post: FormattedPost;
@@ -45,11 +46,47 @@ const Content = ({ post }: Props) => {
   const editor = useEditor({
     extensions: [StarterKit],
     onUpdate: handleOnChangeContent,
+    editorProps: {
+      attributes: {
+        class:
+          'prose prose-sm xl:prose-2xl leading-8 focus:outline-none w-full max-w-full',
+      },
+    },
     content: content,
     editable: isEditable,
   });
 
-  const handleSubmit = async () => {};
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // validation checks
+    if (title === '') setTitleError('This field is required.');
+    if (editor?.isEmpty) setContentError('This field is required.');
+    if (title === '' || editor?.isEmpty) return;
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/post/${post?.id}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: title,
+          content: content,
+        }),
+      }
+    );
+    const data = await response.json();
+
+    handleIsEditable(false);
+    setTempTitle('');
+    setTempContent('');
+
+    setTitle(data.title);
+    setContent(data.content);
+    editor?.commands.setContent(data.content);
+  };
 
   return (
     <div className="prose w-full max-w-full mb-10">
@@ -57,25 +94,18 @@ const Content = ({ post }: Props) => {
       <h5 className="text-wh-300">{`Home > ${post.category} > ${post.title}`}</h5>
 
       {/* CATEGORY AND EDIT */}
-      <div className="flex justify-between items-center">
-        <h4 className="bg-accent-orange py-2 px-5 tex-wh-900 text-sm font-bold">
-          {post.category}
-        </h4>
-
-        <div className="mt-4">
-          {isEditable ? (
-            <div className="flex justify-between gap-3">
-              <button onClick={() => handleIsEditable(!isEditable)}>
-                <XMarkIcon className="h-6 w-6 text-accent-red" />
-              </button>
-            </div>
-          ) : (
-            <button onClick={() => handleIsEditable(!isEditable)}>
-              <PencilSquareIcon className="h-6 w-6 text-accent-red" />
-            </button>
-          )}
-        </div>
-      </div>
+      <CategoryAndEdit
+        isEditable={isEditable}
+        handleIsEditable={handleIsEditable}
+        title={title}
+        setTitle={setTitle}
+        tempTitle={tempTitle}
+        setTempTitle={setTempTitle}
+        tempContent={tempContent}
+        setTempContent={setTempContent}
+        editor={editor}
+        post={post}
+      />
 
       <form onSubmit={handleSubmit}>
         {/* HEADER */}
@@ -85,7 +115,7 @@ const Content = ({ post }: Props) => {
               <textarea
                 className="border-2 rounded-md bg-wh-50 p-3 w-full"
                 placeholder="Title"
-                onChange={(e) => console.log('change title', e.target.value)}
+                onChange={handleOnChangeTitle}
                 value={title}
               />
               {titleError && (
@@ -97,7 +127,7 @@ const Content = ({ post }: Props) => {
           )}
           <div className="flex gap-3">
             <h5 className="font-semibold text-xs">By {post.author}</h5>
-            <h6 className="text-wh-300 text-xs">{post.createdAt}</h6>
+            <h6 className="text-wh-300 text-xs">{formattedDate}</h6>
           </div>
         </>
 
@@ -115,21 +145,14 @@ const Content = ({ post }: Props) => {
           />
         </div>
 
-        <div
-          className={
-            isEditable
-              ? 'border-2 rounded-md bg-wh-50 p-3'
-              : 'w-full max-w-full'
-          }
-        >
-          {isEditable && (
-            <>
-              <EditorMenuBar editor={editor} />
-              <hr className="border-1 mt-2 mb-5" />
-            </>
-          )}
-          <EditorContent editor={editor} />
-        </div>
+        {/* ARTICLE */}
+        <Article
+          contentError={contentError}
+          editor={editor}
+          isEditable={isEditable}
+          setContent={setContent}
+          title={title}
+        />
 
         {/* SUBMIT BUTTON */}
         {isEditable && (
